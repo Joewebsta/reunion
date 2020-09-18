7887e8b31dc0219e46afa8cdf6cf40402632c0aa
Joe Webster <joseph.scott.webster@gmail.com>
2020-09-18 16:49:22 -0400

    Write tests for Activity#add_participant. Write code to pass tests.

--- lib/activity.rb
+++ lib/activity.rb
@@ -7,4 +7,9 @@ class Activity
     @participants = {}
     @total_cost = 0
   end
+
+  def add_participant(name, amt_paid)
+    participants[name] = amt_paid
+    self.total_cost += amt_paid
+  end
 end
--- spec/activity_spec.rb
+++ spec/activity_spec.rb
@@ -1,9 +1,9 @@
 require './lib/activity'
 
 describe Activity do
-  describe '#initialize' do
-    subject { Activity.new('Brunch') }
+  subject { Activity.new('Brunch') }
 
+  describe '#initialize' do
     it 'is an instance of Activity' do
       expect(subject).to be_an_instance_of Activity
     end
@@ -18,6 +18,32 @@ describe Activity do
 
     it 'has a cost total cost of zero' do
       expect(subject.total_cost).to eql(0)
+    end
+  end
+
+  describe '#add_participant' do
+    before { subject.add_participant('Maria', 20) }
+
+    context 'when one participant is added' do
+      it 'creates a new participants hash item' do
+        expect(subject.participants).to eql({ 'Maria' => 20 })
+      end
+
+      it 'updates total_cost' do
+        expect(subject.total_cost).to eql(20)
+      end
+    end
+
+    context 'when a second participant is added' do
+      before { subject.add_participant('Luther', 40) }
+
+      it 'creates a new participants hash item' do
+        expect(subject.participants).to eql({ 'Maria' => 20, 'Luther' => 40 })
+      end
+
+      it 'updates total_cost' do
+        expect(subject.total_cost).to eql(60)
+      end
     end
   end
 end
